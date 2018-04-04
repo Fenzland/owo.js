2176470ea4fdb0817ac82edc9bf9b0dae8eb6d3f
Fenz <uukooo@126.com>
2018-04-04 11:01:31 +0800

    instance of OwO listens events

--- src/OwO.js
+++ src/OwO.js
@@ -1,7 +1,41 @@
+import Listener from './Listener.js';
 
 class OwO
 {
+	constructor()
+	{
+		this.listeners= {};
+	}
 	
+	listen( event, callback, weight=1, )
+	{
+		const listeners= (
+			this.listeners[event] || (
+				this.listeners[event]= []
+			)
+		);
+		
+		const listener= new Listener( callback, weight, );
+		
+		listeners.push( listener, );
+		
+		listeners.sort( ( x, y, )=>(x.weight - y.weight), );
+		
+		return listener;
+	}
+	
+	unlisten( event, listener, )
+	{
+		const listeners= this.listeners[event];
+		
+		if(!(  listeners  )) return;
+		
+		let index= listeners.indexOf( listener, );
+		
+		if(!( index >= 0 )) return;
+		
+		listeners.splice( index, 1, );
+	}
 }
 
 export default OwO;
