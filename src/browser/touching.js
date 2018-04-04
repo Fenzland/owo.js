a1d542ebb06a623a59377b931e74002c259d9e09
Fenz <uukooo@126.com>
2018-04-04 11:21:46 +0800

    implement tap event by understand touch events

--- src/browser/touching.js
+++ src/browser/touching.js
@@ -1,7 +1,9 @@
 
+let touching;
+
 export function start( e, )
 {
-	
+	touching= e;
 }
 export function move( e, )
 {
@@ -9,5 +11,24 @@ export function move( e, )
 }
 export function stop( e, )
 {
+	if(
+		touching.touches.length == 1
+		&&
+		touching.target === e.target
+		&&
+		touching.touches[0].clientX - e.changedTouches[0].clientX < 5
+		&&
+		touching.touches[0].clientY - e.changedTouches[0].clientY < 5
+		&&
+		e.timeStamp - touching.timeStamp < 300
+	) tap( e, );
+}
+
+function tap( e, )
+{
+	const event= new MouseEvent( 'tap', e.changedTouches[0], );
 	
+	event.initEvent( 'tap', true, true, );
+	
+	e.target.dispatchEvent( event, );
 }
